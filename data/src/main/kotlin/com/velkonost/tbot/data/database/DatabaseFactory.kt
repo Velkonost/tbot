8a82ecdb25d727c4b11f791fe384daa0254c7ed6
Artem Klimenko <klimenkocpp@icloud.com>
2026-02-12 22:37:13 +0000

    scaffold: add data/src/main/kotlin/com/velkonost/tbot/data/database/DatabaseFactory.kt

---
+++ data/src/main/kotlin/com/velkonost/tbot/data/database/DatabaseFactory.kt
@@ -0,0 +1,22 @@
+package com.velkonost.tbot.data.database
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.Database
+
+object DatabaseFactory {
+
+    fun init(url: String, user: String, password: String) {
+        val config = HikariConfig().apply {
+            jdbcUrl = url
+            driverClassName = "org.postgresql.Driver"
+            username = user
+            this.password = password
+            maximumPoolSize = 10
+            isAutoCommit = false
+            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+            validate()
+        }
+        Database.connect(HikariDataSource(config))
+    }
+}
